723fec81684bd21e82bb4c2c5791afd3459f906c
Igor Amaral <igordominices@poli.ufrj.br>
2020-04-01 14:56:30 -0300

    criação de papéis pelo admin

--- app/admin/usuarios.rb
+++ app/admin/usuarios.rb
@@ -1,12 +1,19 @@
 ActiveAdmin.register Usuario do
-  permit_params :nome, :admin, :email, :password, :password_confirmation
+  # permit_params :nome, :admin, :email, :password, :password_confirmation 
+  permit_params do
+    permitted = [:nome, :email, :password, :password_confirmation]
+    permitted << :other if current_usuario.admin?
+    permitted
+  end
 
   index do
     selectable_column
     id_column
     column :nome
     column :email
-    column :admin
+    if current_usuario.admin?
+      column :admin
+    end
     actions
   end
 
@@ -17,10 +24,12 @@ ActiveAdmin.register Usuario do
   form do |f|
     f.inputs do
       f.input :nome
-      f.input :admin
+      if current_usuario.admin?
+        f.input :admin
+      end
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :password, :required => true if params[:action] == 'create'
+      f.input :password_confirmation, :required => true if params[:action] == 'create'
     end
     f.actions
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_190134) do
+ActiveRecord::Schema.define(version: 2020_04_01_164905) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_03_30_190134) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "papers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nome"
+    t.string "ticker"
+    t.string "tipo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tipo"], name: "index_papers_on_tipo", unique: true
   end
 
   create_table "usuarios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
